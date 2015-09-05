6ebe6535f02edd69ecd4812fe355b6fc8520f238
emmaoberstein <Emma@Emmas-MacBook-Pro.local>
2015-09-05 01:01:20 -0400

    added logo to launch

---
+++ dumpstr/dumpstr/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  dumpstr
+//
+//  Created by Emma Oberstein on 9/5/15.
+//  Copyright © 2015 PennApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+}
+
