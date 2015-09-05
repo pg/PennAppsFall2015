63d62be35a841e530b69a1bdbc9951276d3b338f
emmaoberstein <Emma@Emmas-MacBook-Pro.local>
2015-09-05 03:02:31 -0400

    date tabs added

--- dumpstr/dumpstr/ViewController.swift
+++ dumpstr/dumpstr/ViewController.swift
@@ -8,19 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet
+    var tableView: UITableView?
+    var items: [String] = ["August 17th, 2015", "August 28th, 2015", "September 4th 2015"]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView!.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    } 
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView!.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+    }
 }
 
